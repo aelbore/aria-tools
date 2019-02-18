511a35a88bc53c08cbf369044fa1a0b88e03236b
aelbore <aelbore.sg@gmail.com>
2019-02-18 20:15:26 +0800

    update build.ts and update dependencies

--- build.ts
+++ build.ts
@@ -1,18 +1,21 @@
-const path = require('path')
 const fs = require('fs')
 
 const { promisify } = require('util')
-const { clean }= require('aria-fs')
+const { join, resolve, dirname } = require('path')
 
-const { ROLLUP_TYPESCRIPT_OPTIONS, typescript2, resolve } = require('./packages/test/utils')
-const { rollupBuild, copyBinFiles, copyPackageJson, copyReadme } = require('./packages/common/index')
+const { clean, globFiles, mkdirp } = require('aria-fs')
+const { rollup } = require('rollup')
+
+const typescript2 = require('rollup-plugin-typescript2')
+const nodeResolve = require('rollup-plugin-node-resolve')
 
 const copyFile = promisify(fs.copyFile)
+const writeFile = promisify(fs.writeFile)
 
-const DIST_PATH = path.join('dist', 'test')
-const TEST_PACKAGE = path.join('packages', 'test')
+const DIST_PATH = join('dist', 'test')
+const TEST_PACKAGE = join('packages', 'test')
 
-const ENTRY_FILE = path.join(TEST_PACKAGE, 'index.ts')
+const ENTRY_FILE = join(TEST_PACKAGE, 'index.ts')
 
 const rollupConfig = {
   inputOptions: {
@@ -26,20 +29,65 @@ const rollupConfig = {
       'rollup-plugin-node-resolve'
     ],
     plugins: [
-      typescript2({ ...ROLLUP_TYPESCRIPT_OPTIONS }),         
-      resolve()
+      typescript2({
+        tsconfigDefaults: { 
+          compilerOptions: {
+            "baseUrl": ".",
+            "emitDecoratorMetadata": true,
+            "experimentalDecorators": true,
+            "target": "es2015", 
+            "module": "es2015", 
+            "moduleResolution": "node",           
+            "lib": [ 
+              "dom", 
+              "es2015", 
+              "es2017"  
+            ]       
+          }
+        },
+        check: false,
+        cacheRoot: join(resolve(), 'node_modules/.tmp/.rts2_cache'), 
+        useTsconfigDeclarationDir: true
+      }),         
+      nodeResolve()
     ]
   },
   outputOptions: {
     sourcemap: false,
-    file: path.join(DIST_PATH, 'test.js'),
+    file: join(DIST_PATH, 'test.js'),
     format: 'cjs'
   } 
 }
 
+async function copyBinFiles() {
+  const files = await globFiles('bin/**/*')
+  return Promise.all(files.map(file => {
+    const destFile = file.replace(resolve(), 'dist') 
+    mkdirp(dirname(destFile))
+    return copyFile(file, destFile)
+  }))
+}
+
+async function copyPackageJson() {
+  const pkg = require('./package.json');
+  pkg.paths.test = '../test/test'
+  delete pkg.scripts;
+  delete pkg.devDependencies;
+  return writeFile(join('dist', 'package.json'), JSON.stringify(pkg, null, 2))
+}
+
+ async function copyReadme() {
+  const README = 'README.md'
+  return copyFile(resolve(README), resolve(join('dist', README)))
+}
+
 async function copyKarmaRollup() {
-  const { join } = path, FILE_NAME = 'karma-rollup.js'
+  const FILE_NAME = 'karma-rollup.js'
   return copyFile(join(TEST_PACKAGE, FILE_NAME), join(DIST_PATH, FILE_NAME)) 
+}
+
+async function rollupBuild({ inputOptions, outputOptions }) {
+  return rollup(inputOptions).then(bundle => bundle.write(outputOptions));
 }
 
 clean('dist')
--- packages/common/copy.ts
+++ packages/common/copy.ts
@@ -2,29 +2,7 @@ import * as path from 'path'
 import * as util from 'util'
 import * as fs from 'fs'
 
-import { globFiles, mkdirp } from 'aria-fs';
-
 const copyFile = util.promisify(fs.copyFile)
-const writeFile = util.promisify(fs.writeFile)
-
-export async function copyBinFiles() {
-  const { resolve, dirname } = path
-  const files = await globFiles('bin/**/*')
-  return Promise.all(files.map(file => {
-    const destFile = file.replace(resolve(), 'dist') 
-    mkdirp(dirname(destFile))
-    return copyFile(file, destFile)
-  }))
-}
-
-export async function copyPackageJson() {
-  const { resolve, join } = path
-  const pkg = require(resolve('package.json'));
-  pkg.paths.test = '../test/test'
-  delete pkg.scripts;
-  delete pkg.devDependencies;
-  return writeFile(join('dist', 'package.json'), JSON.stringify(pkg, null, 2))
-}
 
 export async function copyReadme() {
   const { resolve, join } = path, README = 'README.md'
