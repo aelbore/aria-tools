1df30ff4685276041a8cce5db2ae6c2c53618c5f
aelbore <aelbore.sg@gmail.com>
2019-02-18 09:06:46 +0800

    refactor symlink

--- symlink.js
+++ symlink.js
@@ -2,9 +2,12 @@
   const { join, resolve } = require('path')
   const { existsSync } = require('fs')
   const { symlinkDir } = require('aria-fs')
+  const { exec } = require('child_process');
 
   const NODE_MODULES = resolve(join(__dirname, 'node_modules'))
   if (!(existsSync(NODE_MODULES))) {
     await symlinkDir(resolve('../'), NODE_MODULES)
+    await exec('./node_modules/.bin/ts-node build.ts')
   } 
+
 })()
